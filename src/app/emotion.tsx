68b09e16bba498ea7baf399eb07a3e4063dd1a84
crazycheese <sjj518588@sina.com>
2025-04-09 21:18:45 +0800

    feat: init mui

---
+++ src/app/emotion.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import { CacheProvider } from '@emotion/react';
+import createEmotionCache from '../lib/emotionCache';
+
+export default function EmotionRegistry({ children }: { children: React.ReactNode }) {
+  const cache = React.useMemo(() => createEmotionCache(), []);
+  return <CacheProvider value={cache}>{children}</CacheProvider>;
+}
